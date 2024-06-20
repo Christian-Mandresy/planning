aec73f746d82c6251754dc5721fbd5e2672847d9
Christian-Mandresy <89293572+Christian-Mandresy@users.noreply.github.com>
2024-06-20 21:53:18 +0300

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/planningProduction/';
+$config['base_url'] = 'https://planningcircuit.netlify.app/planning/';
 
 
 /*
@@ -36,7 +36,7 @@ $config['base_url'] = 'http://localhost/planningProduction/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = 'form-login';
 
 /*
 |--------------------------------------------------------------------------
